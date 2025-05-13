8e84c3fb312b3d10c26815d03f8482a139732cce
mnapo <napolimatiase@gmail.com>
2025-05-13 02:22:20 -0300

    create SourceAdapter class

---
+++ src/cls/SourceAdapter.lua
@@ -0,0 +1,25 @@
+local ClassPrototype = require("ClassPrototype")
+local SourceAdapter = ClassPrototype:new()
+SourceAdapter.__index = SourceAdapter
+
+local TYPES = {
+    "HTML",
+    "solar2D"
+}
+
+function SourceAdapter:new(parent, type, configuration)
+    local instance = ClassPrototype:new()
+
+    local type = type or TYPES[1]
+    local configuration = configuration or {}
+
+    instance:set("parent", parent)
+    :set("type", type)
+    :set("configuration", configuration)
+
+    instance = setmetatable(instance, self)
+    instance.__index = self
+    return instance
+end
+
+return SourceAdapter
