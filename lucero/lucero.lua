8edb4ff71969ddaa293e75fafa43b41be2946e67
mnapo <napolimatiase@gmail.com>
2025-09-19 18:13:43 -0300

    expose updateById and getNodeById

--- lucero/lucero.lua
+++ lucero/lucero.lua
@@ -4,11 +4,23 @@ local Renderer = require("lucero.core.renderer")
 local Lucero = {}
 
 function Lucero.createElement(type, props, children)
-    return Element.create(type, props, children)
+    local ch = {}
+    if type(children) == "table" then
+        ch = children
+    end
+    return Element.create(type, props, ch)
 end
 
 function Lucero.render(element, target)
     return Renderer.render(element, target)
+end
+
+function Lucero.updateById(id, newProps)
+    return Renderer.updateById(id, newProps)
+end
+
+function Lucero.getNodeById(id)
+    return Renderer.getNodeById(id)
 end
 
 return Lucero
