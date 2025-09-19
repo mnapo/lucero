51a0cfca481fdeee5016e49297aae43d555c9d83
mnapo <napolimatiase@gmail.com>
2025-09-19 18:13:00 -0300

    allow fontSize and color update

--- lucero/targets/solar2d/text.lua
+++ lucero/targets/solar2d/text.lua
@@ -5,10 +5,12 @@ function Text.create(props)
         text = props.text or "",
         x = props.x or display.contentCenterX,
         y = props.y or display.contentCenterY,
-        font = native.systemFont,
+        font = props.font or native.systemFont,
         fontSize = props.size or 20
     })
-    txt:setFillColor(1, 1, 1)
+    if props.color then
+        txt:setFillColor(unpack(props.color))
+    end
     return txt
 end
 
@@ -16,10 +18,14 @@ function Text.update(node, props)
     if props.text then node.text = props.text end
     if props.x then node.x = props.x end
     if props.y then node.y = props.y end
+    if props.size then node.size = props.size end
+    if props.color then node:setFillColor(unpack(props.color)) end
 end
 
 function Text.remove(node)
-    node:removeSelf()
+    if node and node.removeSelf then
+        node:removeSelf()
+    end
 end
 
 return Text
